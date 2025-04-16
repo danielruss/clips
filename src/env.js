5433ffb08fa45f8ad1936b4a73bd18fc47283884
Praful Bhawsar <prtsh32@gmail.com>
2025-04-15 22:01:05 -0400

    Fix for Rollup packaging

--- src/env.js
+++ src/env.js
@@ -7,23 +7,8 @@ const IS_WEBNN_AVAILABLE = typeof navigator !== 'undefined' && 'ml' in navigator
 const IS_PROCESS_AVAILABLE = typeof process !== 'undefined';
 const IS_NODE_ENV = IS_PROCESS_AVAILABLE && process?.release?.name === 'node';
 
-export const device = IS_NODE_ENV?"cpu":(IS_WEBGPU_AVAILABLE?"webgpu":"wasm");
-export async function getOnnxRuntime(){
-    let ort;
-    if (IS_NODE_ENV) {
-        ort = await import('onnxruntime-node')
-    } else {
-        ort =  await import('onnxruntime-web')
-    }
+export const device = IS_NODE_ENV ? "cpu":(IS_WEBGPU_AVAILABLE?"webgpu":"wasm");
 
-    return ort;
-}
+export const ort = IS_NODE_ENV ? await import('onnxruntime-node') : await import('onnxruntime-web')
 
-/*
-ort.env.wasm.wasmPaths = {
-    'ort-wasm-simd-threaded.jsep.wasm': './dist/browser/ort-wasm-simd-threaded.jsep.wasm',
-    'ort-wasm-simd.wasm': './dist/browser/ort-wasm-simd.wasm',
-    'ort-wasm-threaded.wasm': './dist/browser/ort-wasm-threaded.wasm',
-}
-console.log("set wasm path ====>",ort.env.wasm.wasmPaths)
-*/
+ort.env.wasm.wasmPaths = 'https://cdn.jsdelivr.net/npm/onnxruntime-web/dist/'
