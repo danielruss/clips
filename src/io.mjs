583245602a2b0eaa8c11fdeb55ebccfcf2a0eccd
Daniel Russ <druss@mail.nih.gov>
2025-04-21 18:25:45 -0400

    use my npm

--- src/io.mjs
+++ src/io.mjs
@@ -1,17 +1,4 @@
-let Papa;
-async function init() {
-    if (typeof window === 'undefined') {
-        // Node.js environment
-        console.log('IO Running in Node.js');
-        const {default:p} = await import('papaparse');
-        Papa=p;
-    } else {
-        // Browser environment
-        console.log('Running in Browser');
-        Papa = await import('https://cdn.jsdelivr.net/npm/papaparse@5.5.2/+esm');
-    }
-}
-await init();
+import * as Papa from 'papaparse'
 
 
 export async function read_csv(file){
